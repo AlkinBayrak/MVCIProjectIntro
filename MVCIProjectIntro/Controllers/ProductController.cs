0c7813f2fe56d08791d6873a86e75f0d4fd361c1
AlkinBayrak <alkinbayrak01@gmail.com>
2024-10-23 17:21:15 +0300

    Add project files.

---
+++ MVCIProjectIntro/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCIProjectIntro.Models;
+
+namespace MVCIProjectIntro.Controllers
+{
+    public class ProductController : Controller
+    {
+        private static List<Product> list = new List<Product>()
+        {
+            new Product {Id = 1 ,Name = "Kek",Price = 15},
+            new Product {Id = 2 ,Name = "Elma",Price = 150},
+            new Product {Id = 3 ,Name = "Limon",Price = 250}
+        };
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult List()
+        {
+            return View(list)
+;       }
+    }
+}
